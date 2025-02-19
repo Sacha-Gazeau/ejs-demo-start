cb1ea2d28dd871e35ed067beedb419384cc81039
Sacha-Gazeau <sacha.gazeau@student.arteveldehs.be>
2025-02-19 15:29:27 +0100

    index.ejs content add

--- src/controllers/dinoController.js
+++ src/controllers/dinoController.js
@@ -1,14 +1,14 @@
 import { dinosaurs, navItems } from "../data/data.js";
 
 export const index = (req, res) => {
-  res.render("dinosaurs/dinosaurs", {
+  res.render("dinosaurs/index", {
     navItems,
     dinosaurs,
   });
 };
 
 export const details = (req, res) => {
-  res.render("dinosaurs/dinosaurs", {
+  res.render("dinosaurs/details", {
     navItems,
     dinosaurs,
   });
